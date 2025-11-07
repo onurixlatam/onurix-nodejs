1ae8c509d583f4697a3f3ad1c59131549365d9db
Jhoan Zambrano <alejo.ax1306@gmail.com>
2025-11-07 13:07:40 -0500

    resolved #13

--- GroupsAndContacts/GroupList.js
+++ GroupsAndContacts/GroupList.js
@@ -1,11 +1,11 @@
 // Ejecutar: npm install axios --save
-const axios = require('axios').default;
+const axios = require('axios').defaults;
 let headers={
      headers:{
           'content-type': 'application/x-www-form-urlencoded',
      }
 }
-axios.get('https://www.onurix.com/api/v1/group/list?key=AQUI_SU_KEY&client=AQUI_SU_ID',headers)
+axios.get('https://www.onurix.com/api/v1/group/list?key=AQUI_SU_KEY&client=AQUI_SU_ID&page=AQUI_NUMERO_PAGINA',headers)
           .then(resp=>{
                console.log(resp.data);
           }).catch(error=>{
